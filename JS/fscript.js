d224cf0e91b71c21b706177cdff29cdfce362828
alexandarcad01 <alexandaracad01@gmail.com>
2024-01-06 15:33:50 +0530

    all files added

--- JS/fscript.js
+++ JS/fscript.js
@@ -1,98 +1,100 @@
-alert("welcome to js");
-
-console.log("welcome to js");
+//to use console check the below line
+console.log("Welcome to javascript");
+console.log('Welcome to javascript1');
 console.log(10);
 console.log(10.10);
-console.log({fname:"Alex",gender:"male"});
 
-console.table({fname:"Alex",gender:"male"});
-//print Array
-console.log([5,10,25,15]);
-console.table([5,10,25,15]);
-//to print Boolean
+//to print objects in console
+console.log({fname:"Alex", gender:"Male"});
+console.table({fname:"Alex", gender:"Male"});
+
+//to print Array in console
+console.log([5,10,20,15]);
+console.table([5,10,20,15]);
 console.log(true);
-//print in console with variable decleration
+
+
+// print in console with variable declartion
+
+//var a;
+//let b;
 var a=5;
 let b=7;
 const c=10;
+
 console.log(a);
 console.log(b);
 console.log(c);
 
-//reasign
-
 a=6;
 console.log(a);
-
 b=8;
 console.log(b);
-//re-declearation
+console.log(c);
 
-var a=25;
+a=6.5;
 console.log(a);
-
-var a=6.5;
-console.log(a);
-
 b=8.5;
 console.log(b);
 
-var a="Alex";
-console.log(a);
 
-b="Ram";
+a="Alex";
+console.log(a);
+b="RAMA";
 console.log(b);
 
-b=25;
-console.log(b);
-// you cannot redecleration variable used in "let" method
-// you can redecleration variable used in "var" method
-var a=25;
-console.log(a);
+// you can redeclare variable used in "Var" method
+//var a=25;
+//console.log(a);
+// you cannot redeclare variable used in "let" method 
+//let b=25;
+//console.log(b);
 
-//array redecleare
-a={fname:"Alex",gender:"male"}
+
+a={fname:"Alex", gender:"Male"};
 console.log(a);
 console.table(a);
+
+b={fname:"Alex", gender:"Male"};
+console.log(b);
+console.table(b);
+
 
 a=[12,11,25,60];
 console.log(a);
 console.table(a);
 
-b={fname:"Alex",gender:"male"}
-console.log(b);
-console.table(b);
 
 b=[12,11,25,60];
 console.log(b);
 console.table(b);
 
-//console page clear
-//to know what type of values given
+// to know what type of values given
+
 a=1;
 console.log(typeof a);
 
 a=1.5;
 console.log(typeof a);
 
+
 a="Raguvaran";
-console.log(a+";"+typeof a);
+console.log(a+" : "+typeof a);
 
-a="true";
-console.log(a+";"+typeof a);
+a=true;
+console.log(a+" : "+typeof a);
 
-//array
 a=[12,15,10];
-console.log(a+";"+typeof a);
-
-//object
-a-{fname:"alex",gender:"Male"};
 console.log(typeof a);
 
-//type conversion
-a=10;
+
+a={fname:"santhosh", gender:"Male"};
 console.log(typeof a);
 
+// type conversion
+
+a=20;
+console.log(typeof a);
 a=a.toString();
 console.log(typeof a);
 
@@ -100,21 +102,12 @@ a=true;
 console.log(typeof a);
 
 a=parseInt(a);
-var d=parseInt(a);
 console.log(typeof a);
 console.log(a);
+
 a=parseFloat(a);
 console.log(typeof a);
 
-a="60"
-d=55;
-console.log(typeof a);
-console.log(a);
-a=parseInt(a);
-console.log(typeof a);
-a=a+d;
-console.log(a);
-
 
 a="60"
 d=55;
@@ -122,25 +115,88 @@ a=a+d;
 console.log(a);
 
 a="60"
-a=number(a);
+
 d=55;
-d=string(d);
+d=String(d);
+a=Number(a);
 a=a+d;
 console.log(a);
 
-var x ="true";
-console.log(x,typeof x);
+var x =true;
+console.log(x, typeof x);
+x=Number(x);
+console.log(x, typeof x);
 
-var x ="25";
-console.log(x,typeof x);
+// const a=10;
 
-x=number(x);
-x=parseInt(x);
-console.log(x,typeof x);
+const f={name:"Alex ", age:30}
+console.log(f, typeof f);
+f.name="RAMA";
+console.log(f, typeof f);
 
-
-
-
-
+f.name=25;
+console.log(f, typeof f);
 
 
+
+
+//OPERATORS in JAVASCRIPT
+
+//arithmatic operator
+
+let v1=10;
+let v2=20;
+//addition
+console.log(v1+v2);
+//subtraction
+console.log(v1-v2);
+//multiply
+console.log(v1*v2);
+//division
+console.log(v1/v2);
+//modulus
+console.log(v1%v2);
+//Expo
+console.log(v1**v2);
+//increment #pre
+v1=++v1;
+console.log(v1);
+//decrement #pre
+v1=--v1;
+console.log(v1);
+
+//assignment operators
+
+v1=25;
+console.log(v1);
+
+v1+=20; // v1=v1+20;
+console.log(v1);
+v1-=20; // v1=v1-20;
+console.log(v1);
+v1*=20; // v1=25*20;
+console.log(v1);
+v1/=20; // v1=v1/20;
+console.log(v1);
+v1%=20; // v1=v1%20;
+console.log(v1);
+
+
+
+//comparision operator
+
+console.log(v1==5);
+console.log(v1==="5", typeof v1);
+console.log(v1!=1);
+console.log(v1!=="5", typeof v1);
+
+//relational operator
+console.log(v1>1);
+console.log(v1<1);
+console.log(v1>=5);
+console.log(v1<=10);
+//logical operator
+v1="s";
+console.log(7==5 || 7<2 || 7>5);
+console.log(v1=="s");
+console.log(!(v1=="s"));
