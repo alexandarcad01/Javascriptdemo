2ceb4db678256c034df9042f97ce851c8cf906f4
alexandarcad01 <alexandaracad01@gmail.com>
2024-01-19 15:25:31 +0530

    all files added

--- JS/session18124.js
+++ JS/session18124.js
@@ -1,5 +1,38 @@
-function GetResultfun()
-{
-    var firstname=document.getElementById("inputvalue").Value;
+function GetResultFun(){
+
+    var firstname=document.getElementById("firstname").value;
     console.log(firstname);
+   
+    firstname=Number(firstname);
+
+    // while loop
+
+    while(firstname<20) // 12<20
+    
+    {
+        console.log("firstname"+ firstname+ " is smaller than 20 "); //printed
+        firstname++; // increased
+        //console.log("input "+ x + " is smaller than 20 "); // print
+        
+    }
+
+// do while loop
+    do{
+        console.log("input in do while for  "+ firstname + " is smaller than 20 "); 
+        firstname++;
+    }
+    while(firstname<0)
+
+// for loop
+
+
+
+//for (stat1; state2;state3l)
+//   for(var a=0; a < 20; a++)  {
+//     console.log("input in for loop  "+ a + " is smaller than 20 "); 
+//   }
+for(firstname;firstname <30; firstname++){
+    console.log("input in for loop  "+ firstname + " is smaller than 20 "); 
+}
+
 }
